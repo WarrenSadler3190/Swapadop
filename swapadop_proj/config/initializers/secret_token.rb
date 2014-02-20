b0204472d8a16ea65773001a389e0a3438d3e667
WarrenSadler3190 <warren.sadler@leankit.com>
2014-02-20 09:38:48 -0600

    initial commit

---
+++ swapadop_proj/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwapadopProj::Application.config.secret_key_base = 'ccf4022730ebdb529fbb4ca29c4f05ab08b7deeb048fdf914f24171735b5e7378a9ebeb10cd8334f74af58ad0ed9b9021b61e6808924c398680e3ba00b7f8325'
---
+++ swapadop_proj/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwapadopProj::Application.config.session_store :cookie_store, key: '_swapadop_proj_session'
